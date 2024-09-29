c93ba911a8b64800d0db13e81721211c73d13ff2
Samuel Asher Rivello <63511769+SamuelAsherRivello@users.noreply.github.com>
2024-09-29 10:49:29 -0500

    Add Unity Input System

--- Unity/Assets/Scripts/Runtime/RMC/[MyProject]/Scenes/Scene01_Intro.cs
+++ Unity/Assets/Scripts/Runtime/RMC/[MyProject]/Scenes/Scene01_Intro.cs
@@ -1,5 +1,6 @@
 using RMC.MyProject.UI;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 namespace RMC.MyProject.Scenes
@@ -26,23 +27,69 @@ namespace RMC.MyProject.Scenes
         [SerializeField]
         private HudUI _hudUI;
 
+        [SerializeField]
+        private Rigidbody _playerRigidBody;
 
+        [SerializeField]
+        private float _moveSpeed = 3;
+
+        [SerializeField] 
+        private float _jumpSpeed = 5;
+
+        
+        // 2. These variables are to hold the Action references
+        InputAction _moveInputAction;
+        InputAction _jumpInputAction;
+        InputAction _resetInputAction;
+        
         //  Unity Methods ---------------------------------
         protected void Start()
         {
             Debug.Log($"{GetType().Name}.Start()");
             
+            
+            // Input
+            _moveInputAction = InputSystem.actions.FindAction("Move");
+            _jumpInputAction = InputSystem.actions.FindAction("Jump");
+            _resetInputAction = InputSystem.actions.FindAction("Reset");
+            
             // Set UI Text
             HudUI.SetScore("Score: 000");
             HudUI.SetLives("Lives: 003");
-            HudUI.SetInstructions("Instructions...");
+            HudUI.SetInstructions("Instructions: Arrows, Spacebar, R");
             HudUI.SetTitle(SceneManager.GetActiveScene().name);
         }
 
 
         protected void Update()
         {
+            Vector2 moveInputVector2 = _moveInputAction.ReadValue<Vector2>();
+            
+            if (moveInputVector2.magnitude > 0.1f)
+            {
+                Vector3 moveInputVector3 = new Vector3
+                (
+                    moveInputVector2.x,
+                    0,
+                    moveInputVector2.y
+                );
+                
+                // Move with arrow keys / WASD / gamepad
+                _playerRigidBody.AddForce(moveInputVector3 * _moveSpeed, ForceMode.Force);
+            }
 
+            if (_jumpInputAction.WasPerformedThisFrame())
+            {
+                // Jump with spacebar / gamepad
+                _playerRigidBody.AddForce(Vector3.up * _jumpSpeed, ForceMode.Impulse);
+            }
+            
+            if (_resetInputAction.IsPressed())
+            {
+                // Reload the current scene with R key 
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+            
         }
 
 
